64084a58e0323e26ab11424ad19effcd6bbe47ad
joyshy <joyshy14@gmail.com>
2022-08-01 16:14:10 +0530

    reinitialized files

---
+++ src/components/Note.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { MdDeleteOutline } from "react-icons/md";
+
+const Note = ({ id, text, date, handleDeleteNote }) => {
+  return (
+    <div className="note">
+      <span>{text}</span>
+      <div className="note-footer">
+        <small>{date}</small>
+        <MdDeleteOutline
+          onClick={() => handleDeleteNote(id)}
+          className="delete-icon"
+          size="1.3em"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Note;
